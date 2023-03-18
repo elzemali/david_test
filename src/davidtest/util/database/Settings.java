ed4cccf5e8d04033c1474354d1ea5f0759ac5643
elzemali <lzemali.usthb@gmail.com>
2023-03-18 18:57:08 +0100

    version 0.2 apres débrief

--- src/davidtest/util/database/Settings.java
+++ src/davidtest/util/database/Settings.java
@@ -6,13 +6,22 @@ package davidtest.util.database;
  * @author lenovo
  */
 public class Settings {
-
     private static final String DEFAULT_SCHEMA;
     private static final String PROJECT_URL;
     private static final String DISTINATION_MYSQL;
     private static final String DISTINATION_URL_MYSQL;
     private static final String USERNAME;
     private static final String PASSWORD;
+    
+    static {
+        DISTINATION_MYSQL = "localhost";
+        USERNAME = "root";
+        PASSWORD = "";
+        DISTINATION_URL_MYSQL = "D:\\wamp64\\bin\\mysql\\mysql5.7.31";
+        DEFAULT_SCHEMA = "test_rec";
+        PROJECT_URL = "C:\\Users\\lenovo\\Documents\\NetBeansProjects\\DavidTest\\src";
+
+    }
 
     public static String getDISTINATION_MYSQL() {
         return DISTINATION_MYSQL;
@@ -37,18 +46,4 @@ public class Settings {
     public static String getDEFAULT_SCHEMA() {
         return DEFAULT_SCHEMA;
     }
-    
-    //TODO ecrire la partie static en haut de comme construteur
-    static {
-        DISTINATION_MYSQL = "localhost";
-        USERNAME = "root";
-        PASSWORD = "";
-        DISTINATION_URL_MYSQL = "D:\\wamp64\\bin\\mysql\\mysql5.7.31";
-        DEFAULT_SCHEMA = "test_rec";
-        PROJECT_URL = "C:\\Users\\lenovo\\Documents\\NetBeansProjects\\DavidTest\\src";
-
-    }
-
-    
-
 }
